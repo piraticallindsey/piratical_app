cc55ebbf144a28fbdaf208b325671989959708ec
YOUR NAME <YOUR@EMAIL.com>
2017-06-21 11:59:38 -0600

    implemented custom scopes for portfolio items

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :portfolios, execpt: [:show]
   
+  get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
