b38a66adf3bfb2f01b586873444ea47579d79236
YOUR NAME <YOUR@EMAIL.com>
2017-06-28 11:44:22 -0600

    added custom layouts for blogs and portfolios

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,11 +1,12 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
-
+  layout "blog"
+  
   # GET /blogs
   # GET /blogs.json
   def index
     @blogs = Blog.all
-    @page_title = 'My Portfolio Blog'
+    @page_title = 'My Blog'
   end
 
   # GET /blogs/1
