6e2640214ef19c57331eeb26bc448633664ba924
YOUR NAME <YOUR@EMAIL.com>
2017-07-17 08:58:53 -0600

    refactored navigation and active status

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,6 @@
 class PortfoliosController < ApplicationController
   before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
-  access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :edit, :update]}, company_admin: :all
+  access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :edit, :update]}, site_admin: :all
   layout "portfolio"
   
     def index
